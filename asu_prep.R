6159ac8c7717b114d9afc597f465217ebeff3562
jowen <jordan.owen47@gmail.com>
2024-06-26 13:11:31 -0500

    working on performance plots

--- asu_prep.R
+++ asu_prep.R
@@ -31,39 +31,39 @@
   # set input directory
   p_dir_in <- ""
 
-  # set export directory
-  p_dir_out <- ""
-
-# performance over time -------------------------------------------------------------------------------------------
-
   # set the sheet location
   p_file_summary <- "https://docs.google.com/spreadsheets/d/16RG3zeKHrEFqAtnK-RoLJFZhu11VcmUo8UDKNUWQ5V8/edit?gid=575332293#gid=575332293"
 
+  # set export directory
+  p_dir_out <- "/Users/jowen/Documents/contract_work/dkdk_getting_smart/data/output/asu_prep/"
+
+# ACT ASPIRE (Grade 9) -------------------------------------------------------------------------------------------
+
   # load the data - it's easiest if we pre-specify the sections of this first sheet that contain different data
-  in_perf_summary <- as.data.table(read_sheet(p_file_summary, sheet = "Data Summary Overview", range = "A7:G11"))
+  in_act_aspire <- as.data.table(read_sheet(p_file_summary, sheet = "Data Summary Overview", range = "A7:G11"))
 
   # make column names workable
-  setnames(in_perf_summary, gsub(" ", "_", tolower(colnames(in_perf_summary))))
+  setnames(in_act_aspire, gsub(" ", "_", tolower(colnames(in_act_aspire))))
 
   # pull out subject
-  in_perf_summary[, subject := tolower(stringr::str_remove(microschool_sp24_act_aspire, "EHS 9 "))]
+  in_act_aspire[, subject := tolower(stringr::str_remove(microschool_sp24_act_aspire, "EHS 9 "))]
 
   # melt
-  perf_long <- melt.data.table(in_perf_summary, id.vars = 'subject',
+  act_aspire_long <- melt.data.table(in_act_aspire, id.vars = 'subject',
                                measure.vars = c('needs_support_(mp)', 'close_(pp)', 'ready_(p)', 'exceeds_(hp)'),
                                variable.factor = F, variable.name = "perf_level", value.name = "pct_stud")
 
   # fix names
-  perf_long[perf_level == 'needs_support_(mp)', perf_level := "1 - Needs Support"]
-  perf_long[perf_level == 'close_(pp)',         perf_level := "2 - Close"]
-  perf_long[perf_level == 'ready_(p)',          perf_level := "3 - Ready"]
-  perf_long[perf_level == 'exceeds_(hp)',       perf_level := "4 - Exceeds"]
+  act_aspire_long[perf_level == 'needs_support_(mp)', perf_level := "1 - Needs Support"]
+  act_aspire_long[perf_level == 'close_(pp)',         perf_level := "2 - Close"]
+  act_aspire_long[perf_level == 'ready_(p)',          perf_level := "3 - Ready"]
+  act_aspire_long[perf_level == 'exceeds_(hp)',       perf_level := "4 - Exceeds"]
 
   # change to factor
-  perf_long[, perf_level := as.factor(perf_level)]
+  act_aspire_long[, perf_level := as.factor(perf_level)]
 
   # plot
-  plot_perf_levels <- ggplot(perf_long, aes(x = stringr::str_to_title(subject), y = pct_stud * 100, fill = perf_level)) +
+  plot_act_aspire <- ggplot(act_aspire_long, aes(x = stringr::str_to_title(subject), y = pct_stud * 100, fill = perf_level)) +
 
     # add bars
     geom_col(position = 'dodge') +
@@ -78,9 +78,158 @@
     theme_bw() +
 
     # make the text match - doesn't currently work and I'm not sure how important it is to pursue
+    theme(text = element_text(size = 16),
+          legend.position = 'bottom') +
+
+    # add labels
+    labs(x = "Subject", y = "Percentage of Students", fill = "Performance Level",
+         title = "ACT Aspire Performance, Grade 9")
+
+
+# ACT (Grade 11) --------------------------------------------------------------------------------------------------
+
+  # load the data - it's easiest if we pre-specify the sections of this first sheet that contain different data
+  in_act_math <- as.data.table(read_sheet(p_file_summary, sheet = "Data Summary Overview", range = "A13:H15"))
+  in_act_ela  <- as.data.table(read_sheet(p_file_summary, sheet = "Data Summary Overview", range = "A17:H19"))
+  in_act_avg  <- as.data.table(read_sheet(p_file_summary, sheet = "Data Summary Overview", range = "A21:I22"))
+
+  # remove identifiers from names
+  setnames(in_act_math, gsub("Math ", "", colnames(in_act_math)))
+  setnames(in_act_ela,  gsub("ELA ", "", colnames(in_act_ela)))
+
+  # stack
+  act_perf <- rbind(in_act_math[, subject := 'Math'], in_act_ela[, subject := 'ELA'])[, `...2` := NULL]
+
+  # change names
+  setnames(act_perf, 'Microschool ACT Proficiency', 'school_year')
+
+  # recode school year
+  act_perf[school_year == 'SY 23', school_year := "2022-23"]
+  act_perf[school_year == 'SY 24', school_year := "2023-24"]
+
+  # melt performance levels long
+  act_perf_levels <- melt.data.table(act_perf,
+                                     id.vars = c('school_year', 'subject'),
+                                     measure.vars = c('MP', 'PP', 'P', 'HP'),
+                                     variable.name = "perf_level",
+                                     variable.factor = F,
+                                     value.name = "pct_stud")
+
+  # change to numeric levels
+  act_perf_levels[perf_level == 'MP', perf_level := "1 - Needs Support"]
+  act_perf_levels[perf_level == 'PP', perf_level := "2 - Close"]
+  act_perf_levels[perf_level == 'P',  perf_level := "3 - Ready"]
+  act_perf_levels[perf_level == 'HP', perf_level := "4 - Exceeds"]
+
+  # plot
+  plot_act_levels <- ggplot(act_perf_levels, aes(x = perf_level, y = pct_stud * 100, fill = subject)) +
+
+    # add bars
+    geom_col(position = 'dodge') +
+
+    # add colors
+    scale_fill_manual(values = c("#38761d", "#999999")) +
+
+    # add labels
+    geom_text(aes(label = pct_stud * 100, y = (pct_stud * 100) / 2),
+              position = position_dodge(width = 0.9), color = 'white') +
+
+    # put school years on two different lines
+    facet_wrap( ~ school_year, nrow = 2) +
+
+    # add a box around the proficient levels
+    annotate("rect", xmin = 2.5, xmax = 4.5, ymin = -1, ymax = 55,
+             alpha = 0, color= "#BF9000") +
+
+    # add label
+    geom_text(label = 'Proficient', x = 3.5, y = 57, color = "#BF9000") +
+
+    # remove background
+    theme_bw() +
+
+    # make the text match - doesn't currently work and I'm not sure how important it is to pursue
+    theme(text = element_text(size = 16),
+          legend.position = 'bottom',
+          strip.background = element_rect(fill = "#FFF2CC")) +
+
+    # add labels
+    labs(x = "Subject", y = "Percentage of Students", fill = "Performance Level",
+         title = "ACT Performance, Grade 11")
+
+  # now let's deal with average scores
+  # add state data
+  # in_act_avg[, state_23 := 17.7]
+
+  # melt long, removing writing because it doesn't contribute to the composite score
+  act_avg <- melt.data.table(in_act_avg,
+                             id.vars = 'Microschool ACT Average Scores',
+                             measure.vars = grep("of 36", colnames(in_act_avg), value = T),
+                             variable.name = "subject",
+                             variable.factor = F,
+                             value.name = 'score')
+
+  # abbreviate names
+  act_avg[, subject := stringr::str_remove(subject, " \\(of 36\\)")]
+
+  # add annotation over the science bar since it's the shortest
+  # act_avg[subject == 'Science']
+
+  # plot,
+  plot_act_scores <- ggplot(act_avg[subject != "Composite"], aes(x = subject, y = score)) +
+
+    # add bars
+    geom_col(fill = "#38761d") +
+
+    # add labels
+    geom_text(aes(label = score, y = score / 2), color = 'white') +
+
+    # add composite
+    geom_hline(yintercept = 22.2, color = "#BF9000") +
+
+    # add label
+    geom_text(aes(x = "Science", y = 24, label = "ASUP Composite \n2023-24"), color = "#BF9000") +
+
+    # add the state average in 2023
+    geom_hline(yintercept = 17.7, color = "#999999") +
+
+    # label state average - could not get this to work with annotate(), despite that being what the error calls for
+    geom_text(aes(x = "Science", y = 19, label = "State Avg. \n2022-23"), color = "#999999") +
+
+    # remove background
+    theme_bw() +
+
+    # make text larger for reports
     theme(text = element_text(size = 16)) +
 
     # add labels
-    labs(x = "Subject", y = "Percentage of Students", fill = "Performance Level")
+    labs(x = "ACT Subject", y = "Avg. Score", title = "ACT Performance, Grade 11, 2023-24 School Year")
 
 
+# Exact Path ------------------------------------------------------------------------------------------------------
+
+  # load data
+
+
+
+# export ----------------------------------------------------------------------------------------------------------
+
+  # ACT ASPIRE performance levels
+  ggsave(filename = "act_aspire_perf_levels.png",
+         plot     = plot_act_aspire,
+         path     = p_dir_out,
+         width    = 8,
+         height   = 6)
+
+  # ACT performance levels
+  ggsave(filename = "act_perf_levels.png",
+         plot     = plot_act_levels,
+         path     = p_dir_out,
+         width    = 6,
+         height   = 8)
+
+  # ACT average scores
+  ggsave(filename = "act_avg_scores.png",
+         plot     = plot_act_scores,
+         path     = p_dir_out,
+         width    = 8,
+         height   = 6)
